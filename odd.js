7ab2c21312ad54762861f1331d3746131b0cfa4c
moe <mbchehade@gmail.com>
2019-11-27 03:24:10 +0000

    odd checker challenge

---
+++ odd.js
@@ -0,0 +1,22 @@
+/* PSEUDO-CODE
+===================================
+-Mail going to Even Numbers
+-Mail going to odd Numbers
+-function that sorts the mails. -function to check whether number is odd or not.*/
+
+function isOdd(num) {
+  if (num % 2 != 0) {
+    return true
+  } else {
+    return false
+  }
+}
+
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
+
+
+
+
+
+
